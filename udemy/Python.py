b9d83f93fb695b5be33e3797994ec1628a96d35f
JGH-Programmer <rlgns927@gmail.com>
2024-06-08 10:35:56 +0900

    Calculation Practice

--- udemy/Python.py
+++ udemy/Python.py
@@ -1,18 +1,35 @@
-K = int(input())
-
-List = []
-
-for i in range(K):
-    word = input()
-    List.append((word,len(word)))
-
-List = set(List)
-List = sorted(List, key = lambda x : x[1])
-
-for i in List:
-    print(i[0])
+def add(n1,n2):
+    return n1+n2
+def minus(n1,n2):
+    return n1-n2
+def divide(n1,n2):
+    return n1/n2
+def multiply(n1,n2):
+    return n1*n2
 
 
+
+
+
+operations = {
+    "+" : add,
+    "-": minus,
+    "/": divide,
+    "*": multiply
+}
+
+num1 = int(input())
+num2 = int(input())
+
+
+for symbol in operations:
+    print(symbol)
+operation_symbol = input()
+
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1,num2)
+
+print(answer)
 
 
 
