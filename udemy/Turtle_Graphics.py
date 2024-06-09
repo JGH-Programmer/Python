95cc4ea78d282acb2f879741b235917bcb611d57
JGH-Programmer <rlgns927@gmail.com>
2024-06-09 23:23:19 +0900

    Turtle_Graphics Practice

---
+++ udemy/Turtle_Graphics.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+
+timmy_the_tuttle = Turtle()
+
+timmy_the_tuttle.shape("turtle")
+
+timmy_the_tuttle.color("red")
+
+
+
+screen = Screen()
+screen.exitonclick()
+
