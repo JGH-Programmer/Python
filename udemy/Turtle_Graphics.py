acb7914307a6eb0e83d24e07343d5c22fe3e5835
JGH-Programmer <rlgns927@gmail.com>
2024-06-10 19:54:54 +0900

    Turtle_Graphics.Practice

--- udemy/Turtle_Graphics.py
+++ udemy/Turtle_Graphics.py
@@ -1,14 +1,35 @@
 from turtle import Turtle, Screen
+import random
+
+Turtle_G = Turtle()
+Turtle_G.shape("turtle")
+Turtle_G.color("Black")
 
 
-timmy_the_tuttle = Turtle()
+Turtle_G.hideturtle()
+Turtle_G.pensize(10)
+Turtle_G.speed(10)
 
-timmy_the_tuttle.shape("turtle")
+move_distance = 25
+for _ in range(300):
+    # 랜덤하게 이동 방향 선택
+    direction = random.choice(['north', 'south', 'east', 'west'])
+    color = (random.random(), random.random(), random.random())
 
-timmy_the_tuttle.color("red")
-
-
+    Turtle_G.color(color)
+    # 선택된 방향으로 거북이 이동
+    if direction == 'north':
+        Turtle_G.setheading(90)  # 북쪽으로 향하도록 설정
+        Turtle_G.forward(move_distance)
+    elif direction == 'south':
+        Turtle_G.setheading(270)  # 남쪽으로 향하도록 설정
+        Turtle_G.forward(move_distance)
+    elif direction == 'east':
+        Turtle_G.setheading(0)  # 동쪽으로 향하도록 설정
+        Turtle_G.forward(move_distance)
+    elif direction == 'west':
+        Turtle_G.setheading(180)  # 서쪽으로 향하도록 설정
+        Turtle_G.forward(move_distance)
 
 screen = Screen()
 screen.exitonclick()
-
